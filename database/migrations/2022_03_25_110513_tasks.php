c2b46c80be5cb87f7c3124264489bc41c39e0df5
traylerphi <traylerphi@gmail.com>
2022-03-25 07:30:09 -0400

    Task basic setup

---
+++ database/migrations/2022_03_25_110513_tasks.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Tasks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_task_id')->unsigned();
+            $table->string('task');
+            $table->integer('order');
+            $table->enum('status', ['Incomplete', 'Complete'])->default('Incomplete');
+            $table->timestamp('due')->nullable();
+            $table->timestamp('completion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
